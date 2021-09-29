8ec314c2a1635d8ab0d90c5e3cc287caa335eada
Noufel AMMARI <nammari@e1r1p9.42.fr>
2021-09-29 15:17:28 +0200

    Added function pointer to parse function

--- minishell.h
+++ minishell.h
@@ -3,62 +3,47 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: noufel <noufel@student.42.fr>              +#+  +:+       +#+        */
+/*   By: nammari <nammari@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/16 16:50:54 by noufel            #+#    #+#             */
-/*   Updated: 2021/09/16 17:50:48 by noufel           ###   ########.fr       */
+/*   Updated: 2021/09/29 15:17:09 by nammari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include "unistd.h"
-# include "stdio.h"
-# include "stdlib.h"
-# include "string.h"
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <stdbool.h>
 
 enum e_arg_type {
-    WORD,
-    OPERATOR,
-    PIPE,
     CMD,
+	CMD_SUFFIX,
+    AND,
+	OR,
+    PIPE,
+	VAR,
+	REDIR_IN,
+	REDIR_HERE_DOC,
+	REDIR_OUT_TRUNC,
+	REDIR_OUT_APPEND,
+	FILE
 };
 
-// struct s_pepe {
-//     char    *value;
-//     int_8     type;
-//     struct  s_pepe *next;
-// };
-
-struct s_pepe {
+typedef struct s_ast {
     char    *value;
     int    type;
     struct  s_pepe *left;
     struct  s_pepe *right;
-}           t_pepe;
+}           t_ast;
 
-/*******************************/
-
-// struct	*
-// {
-// 	struct	*s_or_operator;
-// 	struct	*s_and_operator
-// }	t_pipe_sequence;
-
-// struct	s_pipe_sequence
-// {
-// 	struct	*s_command_name;
-// 	struct	*s_command_suffix;
-// }	t_pipe_sequence;
-
-// struct s_ast
-// {
-// 	struct	*s_pipe_sequence;
-// 	struct	*s_or_operator;
-// 	struct	*s_and_operator;
-// }	t_ast;
-
-
+typedef struct s_token {
+    char    *value;
+    int    type;
+    struct  s_pepe *next;
+}           t_token;
 
 #endif
